c0b46befd96450818c6fa8f755d1ede7f8010d6d
gonza <gonza@ALGG08>
2023-09-22 15:36:23 -0600

    first commit

---
+++ src/pkg2023_itess_ti701_tinv1_agg/Main.java
@@ -0,0 +1,21 @@
+/*
+*ITESS-TICS
+*taller de investigacion
+*Intento de curso de certificacion java 1Z0-808
+*By EJMP
+*22/sep/2023
+*/
+
+package pkg2023_itess_ti701_tinv1_agg;
+
+/**
+ *
+ * @author gonza
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
